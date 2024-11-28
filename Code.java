1b762b81a2c1764756f5a48dba5566343b37b5e9
bilal-qadir123 <bilalqadir2020@gmail.com>
2024-11-28 23:40:56 +0500

    Create Code.java

---
+++ Code.java
@@ -0,0 +1,28 @@
+class StarvationTestCricket {
+    static class Fielder1 extends Thread {
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Fielder1: Stopping  the ball!");
+            }
+        }
+    }
+
+    static class Fielder2 extends Thread {
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Fielder2: Fielder1 always stops it, and Fielder2 rarely gets a chance!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Fielder1 fielder1 = new Fielder1();
+        Fielder2 fielder2 = new Fielder2();
+
+        fielder1.start();
+        fielder2.start(); 
+
+        fielder1.setPriority(Thread.MAX_PRIORITY); 
+        fielder2.setPriority(Thread.MIN_PRIORITY); 
+    }
+} 
